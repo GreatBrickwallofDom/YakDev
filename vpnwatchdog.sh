75d69bc96a55dae280555a2c11eb8b1b69105fa0
GreatBrickwallofDom <42009894+GreatBrickwallofDom@users.noreply.github.com>
2018-08-08 16:21:53 -0600

    Create vpnwatchdog.sh

---
+++ vpnwatchdog.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+#VPN watchdog for YAC-VPN link using NMCLI
+
+#setting the vars
+vpn_host_int=""
+vpn_host_ext=""
+credsfile="/home/pi/openvpn/pass"
+internet="unreachable"
+failedpings=0
+sleeptime=5
+#getting the VPN UUID from NMCLI
+vpn_uuid="$(nmcli -t -f UUID,TYPE con | grep vpn | awk -F: '{print $1}')"
+# END OF VARS
+
+
+
+#wait for the internet to come up, do not try VPNcon until external address
+#of vpn server is reachable
+while [[ $internet == "unreachable" ]]; do
+  sleep $sleeptime
+  ping -c 1 $vpn_host_ext >/dev/null 2>&1
+  if [[ $? -ne 0 ]]; then
+    echo "Ext VPN address not reachable, waiting for cell link. Trying again!"
+  else
+    internet="reachable"
+  fi
+done
+
+#try to initially establish the VPN connection before running the watchdog
+echo "Initial VPN connection attempt..."
+nmcli connection up $vpn_uuid passwd-file $credsfile
+sleep $sleeptime
+
+#run the VPN connection watchdog test/restore in infinite loop reboot/restore after X failed
+while [[ true ]]; do
+  sleep $sleeptime
+
+  #check the connection, if bad try and bring up the connection
+  ping -c 1 $vpn_host_int >/dev/null 2>&1
+  if [[ $? -ne 0 ]]; then   #if ping is nonzero...
+    echo "ping failed trying connection"
+    #failedpings=$[failedpings + 1]
+    nmcli connection up $vpn_uuid passwd-file $credsfile
+    sleep 5
+
+  else
+    echo "vpn happy!   :D"
+    failedpings=0
+  fi
+
+  if [[ $failedpings -gt 4 ]]; then
+    failedpings=0
+    echo "Server is unreachable"
+  fi
+done
