d1392158fee21c336909c3ea1b0334bf330ae35e
GreatBrickwallofDom <42009894+GreatBrickwallofDom@users.noreply.github.com>
2018-08-13 16:00:36 -0600

    Update vpnwatchdog.sh

--- vpnwatchdog.sh
+++ vpnwatchdog.sh
@@ -6,9 +6,9 @@
 #  ¯\_(ツ)_/¯
 
 #setting the vars
-vpn_host_int=""
-vpn_host_ext=""
-credsfile=""
+vpn_host_int="10.138.0.3"
+vpn_host_ext="vpn1.yac.dpsrr.com"
+credsfile="/home/pi/openvpn/pass"
 internet="unreachable"
 failedpings=0
 sleeptime=5
@@ -40,56 +40,63 @@ logsetup
 
 #wait for the internet to come up, do not try VPNcon until external address
 #of vpn server is reachable
-while [[ $internet == "unreachable" ]]; do
-  log "Checking internet connection..."
-  sleep $sleeptime
-  ping -c 1 $vpn_host_ext >/dev/null 2>&1
-  if [[ $? -ne 0 ]]; then
-    log "Ext VPN address not reachable, waiting for cell link. Trying again!"
-  else
-    internet="reachable"
-  fi
-done
+function check_inet() {
+  while [[ $internet == "unreachable" ]]; do
+    log "Checking internet connection..."
+    sleep $sleeptime
+    ping -c 1 $vpn_host_ext >/dev/null 2>&1
+    if [[ $? -ne 0 ]]; then
+      log "Ext VPN address not reachable, waiting for cell link. Trying again!"
+    else
+      internet="reachable"
+    fi
+  done
+}
 
 #try to initially establish the VPN connection before running the watchdog
-log "Initial VPN connection attempt..."
-nmcli connection up $vpn_uuid passwd-file $credsfile
-sleep $sleeptime
+function vpn_up() {
+  log "Initial VPN connection attempt..."
+  nmcli connection up $vpn_uuid passwd-file $credsfile
+  sleep $sleeptime
+}
 
 #run the VPN connection watchdog test/restore in infinite loop reboot/restore after X failed
-while [[ true ]]; do
-  sleep $sleeptime
-  #check the connection, if bad try and bring up the connection
-  ping -c 1 $vpn_host_int >/dev/null 2>&1
-  if [[ $? -ne 0 ]]; then   #if ping is nonzero...
-    log "Internal VPN address not reachable, trying to set up us the bomb."
-    log "Number of consecutive failed attempts: $failedpings"
-    #failedpings=$[failedpings + 1]
-    nmcli connection down $vpn_uuid
+function watchdog() {
+  while [[ true ]]; do
     sleep $sleeptime
-    nmcli connection up $vpn_uuid passwd-file $credsfile
-    sleep 5
+    #check the connection, if bad try and bring up the connection
+    ping -c 1 $vpn_host_int >/dev/null 2>&1
+    if [[ $? -ne 0 ]]; then   #if ping is nonzero...
+      log "Internal VPN address not reachable, trying to set up us the bomb."
+      log "Number of consecutive failed attempts: $failedpings"
+      #failedpings=$[failedpings + 1]
+      log "Bringing the VPN down for reset"
+      nmcli connection down $vpn_uuid
+      log "Trying to bring the connection back, please stand by."
+      sleep $sleeptime
+      nmcli connection up $vpn_uuid passwd-file $credsfile
+      sleep 5
+    else
+      log "vpn happy!   :D"
+      failedpings=0
+    fi
 
-  else
-    log "vpn happy!   :D"
-    failedpings=0
-  fi
+    if [[ $failedpings -gt 9 ]]; then
+      failedpings=0
+      log "Server is unreachable, rebooting"
+      sleep $sleeptime
+      reboot
+    fi
+  done
+}
 
-  if [[ $failedpings -gt 9 ]]; then
-    failedpings=0
-    log "Server is unreachable, rebooting"
-    sleep $sleeptime
-    reboot
-  fi
-done
+#Start of execution
+log "Checking network connection"
+check_inet
+log "Bringing up the VPN"
+vpn_up
+log "Starting the watchdog"
+watchdog
 
 
-log "If you ever see this message some really bad shit happened.... EOF
-   T
- .-"-.
-|  ___|
-| (.\/.)
-|  ,,,'
-| '###
-'----'
-"
+log "If you ever see this message some really bad shit happened.... EOF"
